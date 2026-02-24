ae3ceef4d5c1dee80cd5debcf43370d27f4db119
The Veritas <theveritasbusiness@gmail.com>
2026-02-24 06:25:17 +0530

    final db fix

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,13 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+console.log("USING DATABASE_URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
