3a769e3e514e93364e257e9e8db08eb5c7f5b98e
The Veritas <theveritasbusiness@gmail.com>
2026-03-22 15:35:03 +0530

    fix cors for custom domain

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,9 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://theveritas.netlify.app"
+      "https://theveritas.netlify.app",
+      "https://theveritas.in",
+      "http://theveritas.in"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
