da41037727111961ed6b90211bec5b7e425ea553
Sumit Bhatt <sumitbhatt9958@gmail.com>
2026-06-27 23:28:00 +0530

    Allow localhost:3000 and 127.0.0.1:3000 in CORS

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://theveritas.netlify.app",
